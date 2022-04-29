dc4d2f8a348e35834bba916277ef4115d1f09c59
RaulOn18 <radjaoke18@gmail.com>
2022-04-29 14:22:29 +0700

    change title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,6 +31,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
+          <title>Raulon || Portfolio</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
