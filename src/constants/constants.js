b48270a2b7ad7c7b666a57c9e7e3c270f677d07c
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2022-11-16 01:03:52 +0700

    refactor: 💡 change projects and change image

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,12 @@
 export const projects = [
   {
-    title: "Markdown Editor",
+    title: "Helvetica Blog",
     description:
-      "Working with this markdown editor is a breeze. When you use this markdown editor, you will see that it performs quite well. This project was created using technology that is known for its high quality and consistency.",
-    image: "/images/markdown with typescript.png",
-    tags: ["Electron", "Vite", "ReactJs", "CodeMirror", "Typescript"],
-    source: "https://github.com/RaulOn18/electron-markdown-editor-raulon",
-    visit: "https://google.com",
+      "the fastest, lightest, and most interesting blog I've ever made. Why is that? Because this website uses astro.js, it is very, very fast. and also this website is responsive, and also I have made the application using react native",
+    image: "/images/helvetica-blog.png",
+    tags: ["Astro", "Tailwindcss", "ReactJs", "Headlessui", "Typescript"],
+    source: "https://github.com/RaulOn18/blog-kpi-dt",
+    visit: "https://helvetica-blog.vercel.app",
     id: 0,
   },
   {
@@ -20,10 +20,10 @@ export const projects = [
     id: 1,
   },
   {
-    title: "Daarut Tauhid Blog",
+    title: "Ruang Dakwah Blog",
     description:
       "The goal of this project is to finish the STAI Daarut Tauhiid curriculum. This assignment entails the creation of a blog or website. This project allowed me to learn new things, which was a lot of fun. I generally gather data from servers using REST, but this project is different. GraphQL and GraphCMS were used to construct an API for this project. Of course, it's brand new to me, and I enjoy it a lot.",
-    image: "/images/daarut-tauhid-blog.png",
+    image: "/images/ruangdakwah.png",
     tags: ["GraphQL", "NextJs", "TailwindCss", "Sass", "Moment"],
     source: "https://github.com/RaulOn18/daarut-tauhid-blog-nextjs",
     visit: "https://ruangdakwah.vercel.app",
@@ -41,3 +41,4 @@ export const TimeLineData = [
   { year: 2021, text: "Shared my projects with the world" },
   { year: 2022, text: "Started my own platform" },
 ];
+
